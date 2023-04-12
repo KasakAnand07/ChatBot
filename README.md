64e4663bbe2d3ba8283838c81e3e7da415ea2090
Kasak <kasakanand07@gmail.com>
2023-04-12 18:29:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 'npm install'
+use the above command to install dependencies
+
+# 'npm run dev'
+use the above command to run app
---
+++ app/src/App.jsx
@@ -0,0 +1,110 @@
+import { useState } from 'react'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+
+const API_KEY = "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";
+
+function App() {
+    const [typing, setTyping] = useState(false);
+    const [messages, setMessages] = useState([
+        {
+            message: "Hello, I am ChatGPT!",
+            sender: "ChatGPT"
+        }
+    ])
+
+    const handleSend = async (message) => {
+        const newMessage = {
+            message: message,
+            sender: "user",
+            direction: "outgoing"
+        }
+
+        const newMessages = [...messages, newMessage];  // all the old messages = the new message
+
+        // update our message state
+        setMessages(newMessages);
+          
+        // set a typing indicator
+        setTyping(true);
+        // process message to chatGPT {send it over and see the response}
+        await processMessageToChatGPT(newMessages);
+    }
+
+    async function processMessageToChatGPT(chatMessages) {
+        // chatMessages { sender: "user" or "ChatGPT", message: "The message content here" }
+        // apiMessages { role: "user" or "assistent", content: "The message content is here" }
+
+        let apiMessages = chatMessages.map((messsageObject) => {
+            let role = "";
+            if(messsageObject.sender === "ChatGPT") {
+                role="assistant"
+            } else {
+                role = "user"
+            }
+            return { role: role, content: messsageObject.message }
+        });
+
+        // role: "user" -> a message from the user, "assistant" -> a response from ChatGPT
+        // "system" -> generally one initial message definig HOW we want chatGPT to talk
+
+        const systemMessage = {
+            role: "system",
+            content: "Explain all concepts like I am 10 years old."
+        }
+
+        const apiRequestBody = {
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                systemMessage,
+                ...apiMessages // message1, message2, message3, etc
+            ]
+        }
+
+        await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + API_KEY,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(apiRequestBody)
+        }).then((data) => {
+            return data.json();
+        }).then((data) => {
+            console.log(data);
+            console.log(data.choices[0].message.content);
+            setMessages(
+                [...chatMessages, {
+                    message: data.choices[0].message.content,
+                    sender: "ChatGPT"
+                }]
+            );
+            setTyping(false);
+        });
+
+        }
+    
+
+    return (
+        <div className="App"> 
+            <div style={{ position: "relative", height: "800px", width: "700px"}}>
+                <MainContainer>
+                    <ChatContainer>
+                        <MessageList
+                          scrollBehavior='smooth'
+                          typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing" /> : null}
+                        >
+                            {messages.map((message, i) => {
+                                return<Message key={i} model={message} />
+                            })}
+                        </MessageList>
+                        <MessageInput placeholder='Type message here' onSend={handleSend} />
+                    </ChatContainer>
+                </MainContainer>
+            </div>
+        </div>
+    )
+}
+
+export default App
